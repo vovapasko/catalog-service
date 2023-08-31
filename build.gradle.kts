f34f133f6070208335802c8711da405ee6f490a3
vovapasko <vovapasko1699@gmail.com>
2023-08-31 14:39:59 +0200

    set profile and provided test data

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.run.BootRun
 
 plugins {
     id("org.springframework.boot") version "3.1.2"
@@ -37,4 +38,8 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.named<BootRun>("bootRun"){
+    systemProperty("spring.profiles.active", "testdata")
 }
